d08bd803f0dbb635832a9ec8427cda0b30b3bf72
daniel-defra <daniel.dasilveira@defra.gov.uk>
2025-10-09 12:23:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+  app.use(function (req, res, next) {
+    if (req.session && req.session.cookie) {
+      req.session.cookie.sameSite = "lax";
+      req.session.cookie.secure = true;
+    }
+    next();
+  });
+}
+
 const marked = require("./app/config/markdown");
 
 // Access the Nunjucks environment and add the filter
