76ec84f89442f7553772307d2ed431b115b059e0
daniel-defra <daniel.dasilveira@defra.gov.uk>
2025-10-09 13:31:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
   app.use(function (req, res, next) {
     if (req.session && req.session.cookie) {
-      req.session.cookie.sameSite = "lax";
+      req.session.cookie.sameSite = "none"; // allow cross-site POST redirects
       req.session.cookie.secure = true;
     }
     next();
